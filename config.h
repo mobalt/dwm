8e9e39878b11b37177d193a221678b938721c021
Moises Baltazar <mobaltg@gmail.com>
2019-01-10 02:18:55 -0600

    Successfully added Three Column Layout

--- config.h
+++ config.h
@@ -51,11 +51,13 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "tcl.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "tile []=",      tile },    /* first entry is default */
 	{ "float ><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|||",                tcl },
 };
 
 /* key definitions */
