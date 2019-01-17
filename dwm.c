4c1aa10d44dd8e3fa3b422388457a3a7ab63b22c
Moises Baltazar <mobaltg@gmail.com>
2019-01-16 21:44:00 -0600

    modified autostart directory to go inside ~/.config

--- dwm.c
+++ dwm.c
@@ -1453,8 +1453,8 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd ~/.config/dwm; sh ./autostart_blocking.sh");
+	system("cd ~/.config/dwm; sh ./autostart.sh &");
 }
 
 void
